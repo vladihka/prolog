5e437dec5cdef0fdcbbbb0ca77840642d3f0d7ea
sergey <sergey.gojshik.81@gmail.com>
2021-06-05 17:36:04 +0200

    some

---
+++ projekt_si2021_SI07N.pl
@@ -0,0 +1,29 @@
+rodzic("Alexandr", "Olya").
+rodzic("Masha","Olya").
+rodzic("Alexandr","Alina").
+rodzic("Masha","Alina").
+rodzic("Alexandr","Leha").
+rodzic("Masha","Leha").
+rodzic("Leha","Gleb").
+rodzic("Dasha","Gleb").
+rodzic("Alina","Maks").
+rodzic("Leha","Maks").
+
+
+menczyszna("Alexandr").
+menczyszna("Leha").
+menczyszna("Pavel").
+menczyszna("Dima").
+
+kobieta("Olya").
+kobieta("Alina").
+kobieta("Masha").
+
+
+% X jest ojcem Y, jeśli X jest rodzicem, i oraz jest mężczyzną. 
+ojciec(X,Y):-
+    rodzic(X,Y),menczyszna(X).
+% X jest matka Y, jezeli X jest rodzicem i rowniez jest kobieta 
+matka(X,Y):-
+    parant(X,Y),kobieta(X).
+
