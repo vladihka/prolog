d8ad804449305bdd870163dc062930e29fac563d
sergey <sergey.gojshik.81@gmail.com>
2021-06-05 17:38:35 +0200

    some

--- projekt_si2021_SI07N.pl
+++ projekt_si2021_SI07N.pl
@@ -1,3 +1,4 @@
+% X jest rodzicem Y
 rodzic("Alexandr", "Olya").
 rodzic("Masha","Olya").
 rodzic("Alexandr","Alina").
@@ -9,18 +10,19 @@ rodzic("Dasha","Gleb").
 rodzic("Alina","Maks").
 rodzic("Leha","Maks").
 
-
+% X jest menczyszna
 menczyszna("Alexandr").
 menczyszna("Leha").
 menczyszna("Pavel").
 menczyszna("Dima").
 
+% X jest kobieta
 kobieta("Olya").
 kobieta("Alina").
 kobieta("Masha").
 
 
-% X jest ojcem Y, jeśli X jest rodzicem, i oraz jest mężczyzną. 
+% X jest ojcem Y, jeśli X jest rodzicem, i oraz jest mężczyzną 
 ojciec(X,Y):-
     rodzic(X,Y),menczyszna(X).
 % X jest matka Y, jezeli X jest rodzicem i rowniez jest kobieta 
