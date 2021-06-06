393c7fae08467f200364b91a18eef4741472f407
sergey <sergey.gojshik.81@gmail.com>
2021-06-06 16:00:41 +0200

    some

--- projekt_si2021_SI07N.pl
+++ projekt_si2021_SI07N.pl
@@ -11,7 +11,7 @@ rodzic("Leha","Gleb").
 rodzic("Dasha","Gleb").
 rodzic("Alina","Maks").
 rodzic("Leha","Maks").
-
+rodzic("Ira","Alina").
 % X jest menczyszna
 menczyszna("Alexandr").
 menczyszna("Leha").
@@ -22,19 +22,25 @@ menczyszna("Dima").
 kobieta("Olya").
 kobieta("Alina").
 kobieta("Masha").
-
+kobieta("Ira").
 
 % X jest ojcem Y, jeśli X jest rodzicem, i oraz jest mężczyzną 
 ojciec(X,Y):-
     rodzic(X,Y),menczyszna(X).
 % X jest matka Y, jezeli X jest rodzicem i rowniez jest kobieta 
 matka(X,Y):-
-    parant(X,Y),kobieta(X).
+    rodzic(X,Y),kobieta(X).
 
 % X jest siostra Y jezeli oni ma wspolecznego rodzica Z, oraz jezeli X jest kobieta 
 % i X nie jest rowny Y (czlowiek nie moze byc sam siebie siostre)
 siostra(X,Y):-
     rodzic(Z,X),rodzic(Z,Y),kobieta(X), X\=Y.
-%dziala podobnie ak siostra
+%dziala podobnie jak siostra
 braciszek(X,Y):-
     rodzic(Z,X),rodzic(Z,Y),menczyszna(X), X\=Y.
+% X Jest bacia Y jezeli X jest matka Z, a Z jest matka Y 
+babcia(X,Y):-
+    matka(X,Z),matka(Z,Y).
+
+
+
